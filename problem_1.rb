8563f57b0b6a0ba783726c43dfb288eba52b1087
kimaya-nesarikar <73395955+kimaya-nesarikar-psl@users.noreply.github.com>
2023-05-31 11:32:30 +0530

    Update problem_1.rb

--- problem_1.rb
+++ problem_1.rb
@@ -1,18 +1,38 @@
 #Problem 1
+
+#class 
+class HashOperations 
+
+    #Method for value of key
+    def getValueOfKey(hash,key)
+        hash[key]
+    end
+
+    #Method for adding pairs
+    def addPair(hash,key,value)
+        hash[key]=value
+    end
+
+    #Method for removing values below a limit
+    def removePairs(hash,limit)
+        hash.delete_if{|key,value| value<limit}
+    end      
+end
+
 myHash= Hash.new()
 myHash= {"a"=>1,"b"=>2,"c"=>3,"d"=>4}
+#Creating an object
+hashOp=HashOperations.new
 
 #Get value of key "b"
-puts "\nValue of Hash with key b :#{myHash["b"]}" 
+puts "\nValue of Hash with key b :#{hashOp.getValueOfKey(myHash,"b")}" 
 
 #Add pair "{e:5}"
-myHash["e"]=5
-#store() method to add pair 
-myHash.store("f",6) 
-puts "\nHash after adding pairs {e:5} and {f:6} : "
+hashOp.addPair(myHash,"e",5)
+puts "\nHash after adding pair {e:5}: "
 puts myHash
 
 #Remove pairs with value<3.5
-myHash.delete_if{|key,value| value<3.5}
+hashOp.removePairs(myHash,3.5)
 puts "\nHash after deleting pairs with value<3.5 : "
 puts myHash
