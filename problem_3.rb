56e18a458ae56caa090596fa9a6de660d1592582
kimaya-nesarikar <73395955+kimaya-nesarikar-psl@users.noreply.github.com>
2023-05-31 11:33:45 +0530

    Update problem_3.rb

--- problem_3.rb
+++ problem_3.rb
@@ -1,11 +1,22 @@
 #Problem 3
-#Removes first two and last two char from string 
-def remove_first_last_char(str)
-    str[2..-3]
-end 
+
+#class 
+class StringOperations
+    #Removes first two and last two char from string 
+    def remove_first_last_char(str)
+        if(str.length<=4)
+            return "Invalid input: length less tahn 4"
+        else
+            return str[2..-3]
+        end
+    end 
+end
+
+#Creating object 
+strOp=StringOperations.new
 
 #Input 
 puts "Enter the String : "
 str=gets.chomp
-result=remove_first_last_char(str)
-puts "Resulted String :#{result}"
+result=strOp.remove_first_last_char(str)
+puts "Resulted String : #{result}"
