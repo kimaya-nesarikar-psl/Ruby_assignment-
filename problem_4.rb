5e090a13356531e18e58753ec3ab5ea24fca81ba
kimaya-nesarikar <73395955+kimaya-nesarikar-psl@users.noreply.github.com>
2023-05-31 11:34:15 +0530

    Update problem_4.rb

--- problem_4.rb
+++ problem_4.rb
@@ -1,8 +1,19 @@
 #Problem 4
+
+#Class
+class ArrayOperations
+    #Method for checking color
+    def checkColorPresent(arr,color)
+        arr.include?(color)
+    end
+end
+
 color =["Red", "Green", "Blue", "White"]
+#creating object 
+arrOp= ArrayOperations.new
 
 #Input
 puts "Enter the color: "
 check_color=gets.chomp
-puts "Check if '#{check_color}' in array! #{color.include?(check_color)}"
+puts "Check if '#{check_color}' in array! #{arrOp.checkColorPresent(color,check_color)}"
 
