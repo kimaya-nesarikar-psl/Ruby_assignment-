7f76411b536092cfe57978bcd58b2484e20cdb91
kimaya-nesarikar <73395955+kimaya-nesarikar-psl@users.noreply.github.com>
2023-05-31 11:33:15 +0530

    Update problem_2.rb

--- problem_2.rb
+++ problem_2.rb
@@ -1,13 +1,23 @@
 #Problem 2
+
+#class
+class DataOperations
+    def formatData(contacts,contact_data)
+        index=0
+        contacts.each do |key,value|                                   
+            contacts[key][:email] = contact_data[index][0]
+            contacts[key][:address] = contact_data[index][1]
+            contacts[key][:phone] = contact_data[index][2]
+        index=index+1
+        end
+    end 
+end
+
 contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
                 ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 
-index=0
-contacts.each do |key,value|                                   
-    contacts[key][:email] = contact_data[index][0]
-    contacts[key][:address] = contact_data[index][1]
-    contacts[key][:phone] = contact_data[index][2]
-index=index+1
-end 
+#creating object
+dataOp=DataOperations.new 
+dataOp.formatData(contacts,contact_data)
 puts "\nUpdated Contacts are: #{contacts}" 
